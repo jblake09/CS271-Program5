f2a0ac3da7c9e18564b3d40a9f2ada15552ffc3c
Jeffrey E Blake Jr <jeffrey.e.blake@gmail.com>
2018-11-13 15:30:23 -0600

    Add files via upload

---
+++ PG5.asm
@@ -0,0 +1,24 @@
+TITLE Assignment 5     (PG5.asm)
+; Author:
+; Last Modified:			11/13/2018
+; OSU email address:		blakejef@oregonstate.edu
+; Course number/section:	CS271-400
+; Project Number: 5                Due Date: 11/18/2018
+; Description:	Introduce the program.
+;				Get a user request in the range [min = 10 .. max = 200]
+;				Generate request random integers in the range [lo = 100 .. hi = 999], storing them in consecutive elementsof an array
+;				Display the list of integers before sorting, 10 numbers per line.
+;				Sort the list in descending order (i.e., largest first).
+;				Calculate and display the median value, rounded to the nearest integer.
+;				Display the sorted list, 10 numbers per line.
+INCLUDE Irvine32.inc
+; (insert constant definitions here)
+.data
+; (insert variable definitions here)
+.code
+main PROC
+; (insert executable instructions here)
+exit  ; exit to operating system
+main ENDP
+; (insert additional procedures here)
+END main
