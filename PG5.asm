e480083e3ae569709373f4c41acf3e165c99038e
Jeffrey E Blake Jr <jeffrey.e.blake@gmail.com>
2018-11-16 16:35:17 -0600

    Display, populate and intro fully functional

--- PG5.asm
+++ PG5.asm
@@ -26,13 +26,20 @@ instruct2			BYTE	"displays the original list, sorts the list, and calculates the
 instruct3			BYTE	"median value. Finally, it displays the list sorted in descending order."
 request				DWORD	?	; User inputted number that determines number of random integers to generate
 prompt				BYTE	"How many numbers should be generated? [10 .. 200]: ", 0
+outRange			BYTE	"Invalid Input", 0
 randomNums			DWORD	MAX		DUP(?)
 
 
 .code
 main PROC
 
+	push	OFFSET instruct3
+	push	OFFSET instruct2
+	push	OFFSET instruct1
+	push	OFFSET programTitle
 	call	intro
+	push	OFFSET outRange
+	push	OFFSET prompt
 	push	OFFSET request
 	call	getData
 	push	OFFSET randomNums
@@ -48,33 +55,35 @@ main ENDP
 ; (insert additional procedures here)
 ;*********************************************************************************************
 ;Procedure to introduce the program ,and give user instructions.
-;receives: none
-;returns: none
+;receives: the program title and intructions
+;returns: prints the program title and instructions
 ;preconditions:  none
 ;registers changed: edx
 ;*********************************************************************************************
 intro	PROC
 
+	push	ebp
+	mov		ebp, esp
 ;Display your name and program title on the output screen.
-	mov		edx, OFFSET programTitle
+	mov		edx, [ebp+8]
 	call	WriteString
 	call	CrLf
 	call	Crlf
 	call	CrLf
 ;Give User insructions
-	mov		edx, OFFSET instruct1
+	mov		edx, [ebp+12]
 	call	WriteString
 	Call	CrLf
-	mov		edx, OFFSET instruct2
+	mov		edx, [ebp+16]
 	call	WriteString
 	call	CrLf
-	mov		edx, OFFSET instruct3
+	mov		edx, [ebp+20]
 	call	WriteString
 	Call	CrLf
 	call	CrLf
 
-
-	ret
+	pop		ebp
+	ret		16
 intro	ENDP
 
 ;*********************************************************************************************
@@ -87,16 +96,33 @@ intro	ENDP
 getData	PROC
 
 ;get an integer for CompNum
-
 	push	ebp
 	mov		ebp, esp						;set up stack
 	mov		ebx, [ebp+8]
-	mov		edx, OFFSET prompt				; prompt user
+UserInput:
+	mov		edx, [ebp+12]			; prompt user
 	call	WriteString
 	call	ReadInt							; get users number
 	mov		[ebx], eax						; tore in global variable request
+	jmp		Valid1
+Valid1:
+	cmp		eax, MIN
+	jl		NotValid
+	jmp		Valid2
+Valid2:
+	cmp		eax, MAX
+	jg		NotValid
+	jmp		Valid
+NotValid:
+	mov		edx, [ebp+16]			; warn user input is not valid
+	call	WriteString
+	call	CrLf
+	jmp		UserInput
+Valid:
+
 	pop		ebp
-	ret		4
+	ret		12
+
 getData	ENDP
 
 ;*********************************************************************************************
